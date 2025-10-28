b077cd5edb57767d5003cf6f1ca2a5c08c3a8985
hasanur.12 <162147338+hasanur07@users.noreply.github.com>
2025-10-28 08:53:55 +0000

    update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,31 @@
-import { title } from "@/components/primitives";
+"use client"
+import { useEffect, useRef, useState } from "react";
 
 export default function AboutPage() {
+  const divRef = useRef<HTMLDivElement>(null);
+  const [fontSize,setFontSize] = useState(50);
+  const text = "ABOUT AAKC"
+  const setSize = ()=>{
+    if(!divRef.current) return;
+    const {width} = divRef.current.getBoundingClientRect();
+    const fontSize = width / (text.length / 2);
+    setFontSize(fontSize);
+  }
+  useEffect(()=>{
+    setSize();
+    window.addEventListener("resize",setSize);
+    return ()=>{
+      window.removeEventListener("resize",setSize);
+    }
+  },[text])
   return (
-    <div>
-      <h1 className={title()}>About</h1>
+    <div ref={divRef} className="flex flex-col w-full">
+      <h1 className="spaces-nowrap"
+      style={{
+        fontSize: fontSize,
+        whiteSpace: "nowrap",
+      }}
+      >{text}</h1>
     </div>
   );
 }
